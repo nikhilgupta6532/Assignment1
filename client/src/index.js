56b8f382c580a3922b010e71264b0be54b107836
Nikhil <nikhilgupta6532@gmail.com>
2017-11-17 02:16:02 +0530

    Create and show the posts

---
+++ client/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import PostsIndex from './components/posts_index';
+import reducers from './reducers';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import ReduxPromise from 'redux-promise';
+import PostsNew from './components/posts_new';
+import PostsComment from './components/posts_comment';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/comment" component={PostsComment} />
+          <Route path="/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('.container')
+);
